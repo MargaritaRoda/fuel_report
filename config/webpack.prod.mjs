af33cad622791d938af07354f3588ca8321384e4
MargaritaRoda <MargaritaRoda1984@gmail.com>
2024-05-30 22:29:29 +0300

    feat: added portPic fix webpack config prod

--- config/webpack.prod.mjs
+++ config/webpack.prod.mjs
@@ -1,7 +1,8 @@
 import { merge } from 'webpack-merge';
-import { config } from './webpack.common.js';
+import { config } from './webpack.common.mjs';
 
 export const webpackProd = merge(config, {
   mode: 'production',
   devtool: 'source-map',
 });
+export default webpackProd;
